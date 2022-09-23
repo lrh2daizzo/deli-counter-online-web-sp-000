7605be8249020852d257fe882d1c753978d801d2
lrh2daizzo <herrera.lrh@gmail.com>
2022-09-23 10:14:18 -0500

    done..

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,29 @@
 # Write your code here.
 require 'pry'
 
+def line(line)
+    if line.empty?
+       puts  "The line is currently empty."
+    else
+        current_line = "The line is currently:"
+        line.each_with_index do |e,i|
+            current_line << " #{i+1}. #{e}"
+        end
+        puts current_line
+    end
+end
 
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty? 
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.first}."
+        katz_deli.shift
+        katz_deli
+    end
+end
